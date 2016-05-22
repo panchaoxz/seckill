abcad73380287ac2f2e6bb473b70b07b5486d7fa
raywang <bookast@qq.com>
2016-05-22 23:47:07 +0800

    修改页面找不到bug

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
@@ -69,13 +70,13 @@ public class SeckillServiceImpl implements SeckillService {
     }
 
 
-    @Transactional
     /**
      * 使用注解控制事务的优点:
      * 1.开发团队达成一致约定,明确标注事务方法的编程风格.
      * 2.保证事务方法的执行时间尽可能短,不要穿插其他网络操作RPC/HTTP请求或者玻璃到事务方法外部.
      * 3.不是所有的方法都需要事务.如一些查询的service.只有一条修改操作的service.
      */
+    @Transactional
     public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
         if (StringUtils.isEmpty(md5) || !md5.equals(getMD5(seckillId))) {
             throw new SeckillException("seckill data rewrite");
